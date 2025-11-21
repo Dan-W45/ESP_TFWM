96a4c728b1393b1c02992300f2f378989238f2d6
Dan <danwalsh450@gmail.com>
2025-11-21 21:17:14 +0000

    added ntp function

--- LCD.ino
+++ LCD.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 // https://wokwi.com/projects/321525495180034642
 
 // set the LCD number of columns and rows
@@ -14,6 +16,12 @@ LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 const char* ssid = "IoT_Test";
 const char* ap_pass = "goodlife"; 
 
+WiFiUDP ntpUDP;
+// setup NTP: NTP via UDP, NTP pool, offset(s), update interval(ms)
+NTPClient timeClient(ntpUDP);
+//NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);
+
+// Custom characters
 byte bus[8] = {
   B00000,
   B11111,
@@ -39,61 +47,72 @@ byte clockico[8] = {
 void spinner() {
   static int8_t counter = 0;
   const char* glyphs = "\xa1\xa5\xdb";
-  lcd.setCursor(15, 1);
+  lcd.setCursor(19, 0);
   lcd.print(glyphs[counter++]);
   if (counter == strlen(glyphs)) {
     counter = 0;
   }
 }
 
+void displayTime() {
+    int hour = timeClient.getHours();
+    int minutes = timeClient.getMinutes();
+
+    lcd.setCursor(0, 0);
+    lcd.println(timeClient.getFormattedTime());
+
+    lcd.setCursor(0, 2);
+    lcd.print(hour);
+    lcd.print(":");
+    lcd.print(minutes);
+}
 
 void setup(){
   Serial.begin(115200);
-  // initialize LCD
   lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
-  // create the heart character at location 0 (max 8 allowed)
+  lcd.backlight();  // turn on LCD backlight
+  // create the bus character at location 0 (max 8 allowed)
   lcd.createChar(0, bus);
   lcd.createChar(1, clockico);
-
   // WiFi setup
   lcd.setCursor(0, 0);
-  lcd.print("Connecting to ");
-  lcd.setCursor(0, 1);
-  lcd.print("WiFi ");
+  lcd.print("Connecting to WiFi");
 
   Serial.println();
   WiFi.begin(ssid, ap_pass);
 
-  Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.print(".");
+    delay(250);
+    spinner();
   }
-  Serial.println();
-
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-  delay(2500);
-  lcd.clear();
 
+  lcd.setCursor(0, 1);
+  lcd.print("Fetching time");
+  timeClient.begin();
+  timeClient.update();
+  lcd.setCursor(0, 2);
+  lcd.print("Done!");
+  lcd.clear();
 }
 
 void loop(){
   // set cursor to first column, first row
   lcd.setCursor(0, 0);
   // write custom character 0 to lcd
-  lcd.write(0);
-  lcd.setCursor(18, 0);
-  lcd.write(1);
-  lcd.setCursor(4, 0);
-  lcd.print("Destination:");
-  lcd.setCursor(0, 1);
-  lcd.print("126 Birmingham    04");
-  lcd.setCursor(0, 3);
-  lcd.print(WiFi.localIP());
+  // lcd.write(0);
+  // lcd.setCursor(18, 0);
+  // lcd.write(1);
+  // lcd.setCursor(4, 0);
+  // lcd.print("Destination:");
+  // lcd.setCursor(0, 1);
+  // lcd.print("126 Birmingham    04");
+  // lcd.setCursor(0, 3);
+  // lcd.print(WiFi.localIP());
+  displayTime();
+  delay(250);
 }
 
 
